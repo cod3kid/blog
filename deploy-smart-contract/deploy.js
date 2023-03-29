4c2aed5d1d4d940236f3ee310c751d0e745684fd
cod3kid <djsufail@gmail.com>
2023-03-29 18:12:43 +0530

    Deploying Smart Contract

---
+++ deploy-smart-contract/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+
+const { abi, evm } = require("./compile");
+
+provider = new HDWalletProvider(
+  "REPLACE_WITH_YOUR_MNEMONIC",
+  "REPLACE_WITH_YOUR_INFURA_URL"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
