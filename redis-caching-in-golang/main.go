4e9b6c45ce1b12017bd52a3a71d05fed4e6fa2ee
cod3kid <djsufail@gmail.com>
2023-04-02 23:45:46 +0530

    Redis Caching in Golang: Redis Connection and Setting a Key in Redis

--- redis-caching-in-golang/main.go
+++ redis-caching-in-golang/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
-	"errors"
 	"net/http"
-
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 )
 
 func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, {data:"Hello world"})
+	c.IndentedJSON(http.StatusOK,"Testing");
 }
+
+var ctx = context.Background()
+
 
 func main() {
 	//router setup
 	router := gin.Default()
+
+	redisDB := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	// Add a Key
+	err := redisDB.Set(ctx, "topg", "Andrew Tate", 0).Err()
+    if err != nil {
+        panic(err)
+    }
 
 	//routes
 	router.GET("/books", getBooks)
