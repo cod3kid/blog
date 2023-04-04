d67f7332e3c6c707de5cfe02147eabc82a3b6ce6
cod3kid <djsufail@gmail.com>
2023-04-04 19:51:45 +0530

    Redis Caching in Golang: Calling an API and Unmarshalling the JSON

--- redis-caching-in-golang/main.go
+++ redis-caching-in-golang/main.go
@@ -4,40 +4,60 @@ import (
 	"net/http"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
 )
 
+type Post struct {
+    UserID       int    `json:"userId"`
+    ID     int `json:"id"`
+    Title string `json:"title"`
+    Body    string `json:"body"`
+}
+
 func getBooks(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK,"Testing");
+	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+ 
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return 
+	}
+	
+	var parsedJSONObject []Post
+	json.Unmarshal(body, &parsedJSONObject)
+
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	c.IndentedJSON(http.StatusOK,parsedJSONObject);
 }
 
 var ctx = context.Background()
 
+var redisInstance = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+})
 
 func main() {
-	//router setup
 	router := gin.Default()
 
-	redisInstance := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
-	})
-
-	// Setting a Key
 	err := redisInstance.Set(ctx, "topg", "Andrew Tate", 0).Err()
     if err != nil {
         panic(err)
     }
-	// Getting a Key
+
 	val, err := redisInstance.Get(ctx, "ming").Result()
     if err != nil {
         panic(err)
     }
 
 	fmt.Println(val)
-	//routes
+
 	router.GET("/books", getBooks)
 
-	// Listening to port..  by default it listens to 8080 router.Run()
 	router.Run(":8000")
 }
