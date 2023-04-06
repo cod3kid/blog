e99c6afeebde5a0370222e2c1a0da297e9fd50ce
cod3kid <djsufail@gmail.com>
2023-04-06 18:00:04 +0530

    Redis Caching in Golang: Completed

--- redis-caching-in-golang/main.go
+++ redis-caching-in-golang/main.go
@@ -1,61 +1,67 @@
 package main
 
 import (
-	"net/http"
-	"context"
-	"fmt"
-	"io/ioutil"
-	"time"
-	"encoding/json"
-	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
+        "context"
+        "encoding/json"
+        "fmt"
+        "github.com/gin-gonic/gin"
+        "github.com/redis/go-redis/v9"
+        "io/ioutil"
+        "net/http"
+        "time"
 )
 
 type Post struct {
-    UserID       int    `json:"userId"`
-    ID     int `json:"id"`
-    Title string `json:"title"`
-    Body    string `json:"body"`
+        UserID int    `json:"userId"`
+        ID     int    `json:"id"`
+        Title  string `json:"title"`
+        Body   string `json:"body"`
 }
 
 func getPosts(c *gin.Context) {
-	val, err := redisInstance.Get(ctx, "posts").Result()
-    if err != nil {
-	// Calling the API
-	res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
- 
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return 
-	}
-	
-	// Parsing the JSON
-	var parsedJSONObject []Post
-	json.Unmarshal(body, &parsedJSONObject)
+        val, err := redisInstance.Get(ctx, "posts").Bytes()
+        if err != nil {
+                // Calling the API
+                res, err := http.Get("https://jsonplaceholder.typicode.com/posts")
 
-	// Setting the key
-	redisErr := redisInstance.Set(ctx, "posts", body, 20*time.Second).Err()
-    if redisErr != nil {
-     	panic(redisErr)
-    }
-	c.IndentedJSON(http.StatusOK,parsedJSONObject);	
-    }
+                defer res.Body.Close()
+                body, err := ioutil.ReadAll(res.Body)
+                if err != nil {
+                        return
+                }
 
-	fmt.Printf(val)
-	c.IndentedJSON(http.StatusOK, val);
+                // Parsing the JSON
+                var parsedJSONObject []Post
+                json.Unmarshal(body, &parsedJSONObject)
+
+                // Setting the key
+                redisErr := redisInstance.Set(ctx, "posts", body, 20*time.Second).Err()
+                if redisErr != nil {
+                        panic(redisErr)
+                }
+                fmt.Println("Cache Miss")
+                c.IndentedJSON(http.StatusOK, parsedJSONObject)
+        } else {
+                posts := []Post{}
+                err = json.Unmarshal(val, &posts)
+                if err != nil {
+                        panic(err)
+                }
+                fmt.Println("Cache Hit")
+                c.IndentedJSON(http.StatusOK, posts)
+        }
 }
 
 var ctx = context.Background()
 
 var redisInstance = redis.NewClient(&redis.Options{
-	Addr: "localhost:6379",
+        Addr: "localhost:6379",
 })
 
 func main() {
-	router := gin.Default()
+        router := gin.Default()
 
-	router.GET("/posts", getPosts)
+        router.GET("/posts", getPosts)
 
-	router.Run(":8000")
+        router.Run(":8000")
 }
